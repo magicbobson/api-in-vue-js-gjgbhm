e5fbde8b7a05d643d81417ed86674d5afd6e8a22
magicbobson <magicbobson@users.noreply.github.com>
2023-07-08 12:04:02 +0100

    1 file modified

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,47 +1,59 @@
 <template>
-	<div class="container">
-		<h3>Users:</h3>
-		<table class="table">
-			<thead>
-				<tr>
-					<th scope="col">ID</th>
-					<th scope="col">Name</th>
-					<th scope="col">Email</th>
-					<th scope="col">City</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="user in users" v:bind:key="user.id">
-					<th scope="row">{{user.id}}</th>
-					<td>{{user.name}}</td>
-					<td>{{user.email}}</td>
-					<td>{{user.address.city}}</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+  <div class="container">
+    <h3>Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v:bind:key="user.id">
+          <th scope="row">{{ user.id }}</th>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address.city }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-	import axios from "axios";
+import axios from "axios";
+let id: 0
 
 export default {
-  name: "Users",
-  data() {
-    return {
-      users: null
-    };
-  },
-  created: function() {
-    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
-      this.users = res.data;
-    });
-  }
+	name: "Users",
+	data() {
+		return {
+			users: null,
+			robusers = [
+				{
+					id: id++,
+					name: 'chris',
+					email: 'a@a.com',
+					address: {
+						city: 'the city'
+					}
+				}
+			]
+		};
+	},
+	created: function() {
+		// axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+		//   this.users = res.data;
+		// });
+		this.users = robuser;
+	}
 };
 </script>
 
 <style>
-	<h3 {
-		margin-bottom: 5%;
-	}
+<h3 {
+  margin-bottom: 5%;
+}
 </style>
